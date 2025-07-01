a8a9b437fb4dbd59631ea9729d97de9b26774be8
hahaxiaowai <1062080110@qq.com>
2025-07-01 17:23:47 +0800

    feat: 添加示例子包

---
+++ README-ZH.md
@@ -0,0 +1,13 @@
+# turbo-template
+一个用于 Vue 应用的简单 Turbo 模板
+
+
+## 使用方法
+1. 克隆仓库
+2. 运行 `pnpm install`
+3. 运行 `pnpm dev` 启动开发服务器
+4. 运行 `pnpm build` 构建应用程序
+
+
+## 特殊操作
+- 将 `@night-tea` 替换为您自己的命名空间
---
+++ packages/utils/src/browser.ts
@@ -0,0 +1,9 @@
+export function getQueryByName(name: string, url = window.location.href) {
+  // eslint-disable-next-line
+  name = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
